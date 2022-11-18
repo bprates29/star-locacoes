f477fb36c7bcdfc4a44aee511ed5b802a3febc77
bernardo_prates <bernardo_prates@sicredi.com.br>
2022-11-18 07:45:02 -0300

    Sistema incial - correcoes menu resposivo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -85,6 +85,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('user.carros')" :active="request()->routeIs('user.carros')">
+                {{ __('Carros Repasse') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
