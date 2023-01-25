17ec1487bc6bff892bb2d1fac608b460c3fe65af
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-01-25 18:56:44 -0300

    carrengando um temporizados nas fotos

--- resources/views/livewire/revisoes/createPhotos.blade.php
+++ resources/views/livewire/revisoes/createPhotos.blade.php
@@ -33,6 +33,7 @@
                     <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                         <button type="submit"
                                 wire:loading.attr="disabled"
+                                wire:loading.class="bg-gray"
                                 class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-bold text-white shadow-sm hover:bg-red-700 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                             Adicionar
                         </button>
@@ -40,6 +41,7 @@
                     <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
                         <button wire:click="closeModalPhotosPopover()" type="button"
                                 wire:loading.attr="disabled"
+                                wire:loading.class="bg-gray"
                                 class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-bold text-gray-700 shadow-sm hover:text-gray-700 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                             Close
                         </button>
